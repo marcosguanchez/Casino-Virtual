7e81b1b57fe88ed46b3699cc5244789bb5be706f
agrauoneup <151890785+agrauoneup@users.noreply.github.com>
2024-05-31 03:17:20 -0400

    Add files via upload

---
+++ gamblingloginpage/main.js
@@ -0,0 +1,16 @@
+const btnSingIn = document.getElementbyId("sign-in");
+      btnSingUp = document.getElementById("sign-up");
+      formRegister = document.querySelector(".register")
+      formLogin = document.querySelector(".login");
+
+btnSingIn.addEventListener("click", e=> {
+    formRegister.classList.add("hide");
+    formLogin.classList.remove("hide"); 
+}
+)
+
+btnSingUp.addEventListener("click", e=> {
+    formLogin.classList.add("hide");
+    formRegister.classList.remove("hide"); 
+}
+)
